713299716e63f268187742d9cc70f7091947a0db
Tera Coder <31596711+TerableCoder@users.noreply.github.com>
2019-01-02 10:37:21 -0500

    fix

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const Command = require('command')
-
 module.exports = function LocationInfo(mod) {
-	const command = Command(mod)
- 
-	let enabled = false
+	const command = mod.command || mod.require.command;
+	let enabled = false;
 	
 	mod.hook('C_PLAYER_LOCATION', 5, event => {
-		if(enabled) console.log('Location XYZW [ %d | %d | %d | %d ]', Math.round(event.loc.x), Math.round(event.loc.y), Math.round(event.loc.z), event.w)
+		if(enabled) console.log('Location XYZW [ %d | %d | %d | %d ]', Math.round(event.loc.x), Math.round(event.loc.y), Math.round(event.loc.z), event.w);
 	})
 	
 	mod.hook('S_LOAD_TOPO', 3, event => {
-		if(enabled) console.log('ZoneID', event.zone)
+		if(enabled) console.log('ZoneID', event.zone);
     })
 
 	command.add('location', (arg) => {
         enabled = !enabled
-        command.message('[LocationInfo] ' + (enabled ? 'enabled' : 'disabled'))
+        command.message('[LocationInfo] ' + (enabled ? 'enabled' : 'disabled'));
     })
  }
